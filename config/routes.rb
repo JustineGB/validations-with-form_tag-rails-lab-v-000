be106c30b3e99dce54509a16bebb7640f0e9f925
Justine B <justinegayton@gmail.com>
2018-12-29 21:11:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :authors, only: [:index, :new, :create, :edit, :update]
-  resources :posts
+  resources :posts, only: [:index, :new, :create, :edit, :update]
   get '/authors/:id', to: 'authors#show'
   
   
