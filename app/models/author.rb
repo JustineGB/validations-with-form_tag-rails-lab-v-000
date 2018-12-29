abce848877a26b8514ced2a589c5e819a04b8aef
Justine B <justinegayton@gmail.com>
2018-12-29 21:09:16 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,8 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :email, presence: true
   validates :email, uniqueness: true
+  validates :phone_number, presence: true
   validates :phone_number, length: { is: 10 }
 end
